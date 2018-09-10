6a1b845d29763651c3d9a5049b58573b4e0d63b1
github username <github email address>
2018-09-10 09:36:53 -0400

    done

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,20 @@
+require "pry"
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+   "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    array = []
+    i = 0
+    while i < self.characters.length
+      array << "#{self.characters[i].name} - #{self.shows[i].name}"
+      i += 1
+    end
+    array
+  end
+
 end
